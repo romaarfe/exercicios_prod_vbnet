83678a945f48b1be25e20a61da3a4751fc6ac918
Rodrigo Martins de Araújo Fernandes <romaarfe@gmail.com>
2023-05-23 16:21:32 +0100

    Update DataBaseAccess.vb

--- zRodrigoVBPROD/F12 VB connect BD/DataBaseAccess.vb
+++ zRodrigoVBPROD/F12 VB connect BD/DataBaseAccess.vb
@@ -9,10 +9,7 @@ Public Class DataBaseAccess
 
         Dim SCnova As String = "Data Source=89.1............... "
 
-        Dim SCzRodrigoVBPROD = "Data Source=62.28.39.135,62444;
-                                    Initial Catalog=zRodrigoVBPROD;
-                                    User ID=sa;
-                                    Password=123.Abc;"
+        Dim SCzRodrigoVBPROD = "STRING CONNECTION"
 
         'retornar a que é pretendida:
         Return SCzRodrigoVBPROD
